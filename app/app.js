ff5d0c16187c17c2e206c18a59820d6f1e9e3a22
youdianwuliao <youdianwuliao@dadaozei.com>
2018-09-13 11:10:34 +0800

    修改

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon(__dirname + '/public/favicon.svg'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
