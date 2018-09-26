f65cddccde9c8aadc0507fa6531f3fa8e268e925
youdianwuliao <youdianwuliao@dadaozei.com>
2018-09-26 18:43:13 +0800

    修改配置

--- test/ethDemo.js
+++ test/ethDemo.js
@@ -1,6 +1,6 @@
 var BigNumber = require('bignumber.js');
 
-var Chain3 = require('chain3');
+var Web3 = require('web3');
 
 const abiDecoder = require('abi-decoder'); // NodeJS
 
@@ -12,25 +12,25 @@ var config = {};
 
 // set the default geth port if it's not provided
 
-var chain3 = new Chain3(new Chain3.providers.HttpProvider('http://' + "10.1.1.99" + ':' + "8106"));
+var web3 = new Web3(new Web3.providers.HttpProvider('http://' + "10.1.1.99" + ':' + "8206"));
 //是否链接
-console.log("链接: "+chain3.isConnected());
+console.log("链接: "+web3.isConnected());
 
 //版本号
-console.log("版本号: "+chain3.version.api);
+console.log("版本号: "+web3.version.api);
 
 
-var coinbase = chain3.mc.coinbase;
+var coinbase = web3.eth.coinbase;
 console.log("coinbase: "+coinbase);
 
 
-var mining = chain3.mc.mining;
+var mining = web3.eth.mining;
 console.log("挖矿： "+mining);
 
 
 
-
-
+//
+//
 var daoABI =
     [
         {
@@ -254,7 +254,7 @@ var daoABI =
             "type": "event"
         }
 ];
-var contract = chain3.mc.contract(daoABI).at("0x77b39f90b8ace1eb7835946a7dedbffd92e742c9");
+var contract = web3.eth.contract(daoABI).at("0xb8c77482e45f1f44de1745f52c74426c631bdd52");
 console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract.decimals()+" "+contract.totalSupply() );
 
 // console.log("合约余额 "+contract.balanceOf("0xceb87264de34d2e472efdb77bd3a99bab542424b"));
@@ -262,42 +262,63 @@ console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract
 // console.log("合约余额 "+contract.balanceOf("0x31d75b08c52678438c35dd4d19657f651d580ef3"));
 
 
+//
+// web3.eth.toASCII("0xa9059cbb000000000000000000000000aa7a137d17bd5692abe22c2928d780aa948edd5b000000000000000000000000000000000000000000000000000005b312619764")
 
-// chain3.mc.toASCII("0xa9059cbb000000000000000000000000aa7a137d17bd5692abe22c2928d780aa948edd5b000000000000000000000000000000000000000000000000000005b312619764")
+// var queryBlockNumber = 6190953 ;
+// abiDecoder.addABI(daoABI);
+// // 交易记录
+// web3.eth.getBlock(queryBlockNumber, true, function(error,blockData) {
+//     if(null == error){
+//         console.log("开始解析block");
+//     }
+//     for (txi in blockData.transactions){
+//         tx = blockData.transactions[txi];
+//         if(null == tx.to){
+//             continue;
+//         }
+//         var code  = web3.eth.getCode(tx.to);
+//         if(code == "0x"){
+//             var receipt = web3.eth.getTransactionReceipt(tx.hash);
+//             console.log(txi+" "+receipt.logs.length+" logs tx hash: "+tx.hash+" 普通交易 "+tx.from+" to: "+tx.to+" value:"+tx.value+" 票据是否为空： "+ (receipt.logs.length==0));
+//
+//
+//         }else {
+//             var decodeObj = abiDecoder.decodeMethod(tx.input);
+//             if (typeof(decodeObj) != "undefined") {
+//                 var params = decodeObj.params;
+//                 if(params[0].name == "_to" ){
+//                     console.log(txi+" logs tx hash: "+tx.hash+" 合约交易 合约地址:"+tx.to+" from:"+tx.from+"   toAddress: " + params[0].value+" value:"+ params[1].value);
+//                 }else {
+//                     console.log(txi);
+//                 }
+//             }
+//         }
+//     }
+//
+// })
 
-var queryBlockNumber = 458114;
-abiDecoder.addABI(daoABI);
-chain3.mc.getBlock(queryBlockNumber, true, function(error,blockData) {
-    if(null == error){
-        console.log("开始解析block");
-    }
-    for (txi in blockData.transactions){
-        tx = blockData.transactions[txi];
-        // console.log("交易hash：from: "+tx.from+" to: "+tx.to+" value:"+tx.value+"  input: "+tx.input);
 
-        var code  = chain3.mc.getCode(tx.to);
-        if(code == "0x"){
-            var receipt = chain3.mc.getTransactionReceipt(tx.hash);
-            console.log(" 普通交易 "+tx.from+" to: "+tx.to+" value:"+tx.value+" 票据是否为空： "+ (receipt.logs.length==0));
+var transaction = web3.eth.getTransaction("0x783204435fd7ac6ed5158425730bcfd0cc0ae6ec2eee666ba8a8187a8688e13b");
 
-        }else {
-            var decodeObj = abiDecoder.decodeMethod(tx.input);
-            if (typeof(decodeObj) != "undefined") {
-                var params = decodeObj.params;
-                if(params[0].name == "_to" ){
-                    console.log(" 合约交易 合约地址:"+tx.to+" from:"+tx.from+"   toAddress: " + params[0].value+" value:"+ params[1].value);
-                }
+console.log("ceshi:"+transaction.from+" "+transaction.to+"  " +web3.eth.getCode(transaction.to));
+var contract = web3.eth.contract(daoABI).at(transaction.to);
+console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract.decimals()+" "+contract.totalSupply() );
 
-
-                // console.log("input内容: " + decodeObj.params());
-            }
-        }
-    }
-
-})
-
-
-
+// 交易hash
+// web3.eth.getBlock(queryBlockNumber, false, function(error,blockData) {
+//     if(null == error){
+//         console.log("开始解析block");
+//     }
+//     for (txHash in blockData.transactions){
+//         var transaction = web3.eth.getTransaction(blockData.transactions[txHash]);
+//         console.log("txHash: "+txHash+" "+blockData.transactions[txHash]+"  "+transaction.to);
+//     }
+//
+// })
+//
+//
+//
 
 
 
