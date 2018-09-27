b8b9340d76e28e6303355492d6d33a128d689b74
youdianwuliao <youdianwuliao@dadaozei.com>
2018-09-27 16:18:51 +0800

    修改配置

--- test/ethDemo.js
+++ test/ethDemo.js
@@ -254,56 +254,492 @@ var daoABI =
             "type": "event"
         }
 ];
-var contract = web3.eth.contract(daoABI).at("0xb8c77482e45f1f44de1745f52c74426c631bdd52");
-console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract.decimals()+" "+contract.totalSupply() );
 
-// console.log("合约余额 "+contract.balanceOf("0xceb87264de34d2e472efdb77bd3a99bab542424b"));
-// console.log("合约余额 "+contract.balanceOf("0xcebea457d89d78d679e88dd8745e8ebf92388482"));
-// console.log("合约余额 "+contract.balanceOf("0x31d75b08c52678438c35dd4d19657f651d580ef3"));
+var commonABI = [{
+    "constant": true,
+    "inputs": [],
+    "name": "name",
+    "outputs": [{
+        "name": "",
+        "type": "string"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_spender",
+        "type": "address"
+    }, {
+        "name": "_value",
+        "type": "uint256"
+    }],
+    "name": "approve",
+    "outputs": [{
+        "name": "success",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_from",
+        "type": "address"
+    }, {
+        "name": "_to",
+        "type": "address"
+    }, {
+        "name": "_amount",
+        "type": "uint256"
+    }],
+    "name": "transferFrom",
+    "outputs": [{
+        "name": "success",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "withdraw",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "value",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_value",
+        "type": "uint256"
+    }],
+    "name": "burn",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "addresses",
+        "type": "address[]"
+    }],
+    "name": "disableWhitelist",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "_owner",
+        "type": "address"
+    }],
+    "name": "balanceOf",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "addresses",
+        "type": "address[]"
+    }],
+    "name": "airdrop",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [{
+        "name": "",
+        "type": "string"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "finishDistribution",
+    "outputs": [{
+        "name": "",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "addresses",
+        "type": "address[]"
+    }],
+    "name": "enableWhitelist",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "addresses",
+        "type": "address[]"
+    }, {
+        "name": "amounts",
+        "type": "uint256[]"
+    }],
+    "name": "distributeAmounts",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_to",
+        "type": "address"
+    }, {
+        "name": "_amount",
+        "type": "uint256"
+    }],
+    "name": "transfer",
+    "outputs": [{
+        "name": "success",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "getTokens",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "distributionFinished",
+    "outputs": [{
+        "name": "",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "tokenAddress",
+        "type": "address"
+    }, {
+        "name": "who",
+        "type": "address"
+    }],
+    "name": "getTokenBalance",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "totalRemaining",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "_owner",
+        "type": "address"
+    }, {
+        "name": "_spender",
+        "type": "address"
+    }],
+    "name": "allowance",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_tokenContract",
+        "type": "address"
+    }],
+    "name": "withdrawForeignTokens",
+    "outputs": [{
+        "name": "",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "totalDistributed",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "newOwner",
+        "type": "address"
+    }],
+    "name": "transferOwnership",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "addresses",
+        "type": "address[]"
+    }, {
+        "name": "amount",
+        "type": "uint256"
+    }],
+    "name": "distribution",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "name": "blacklist",
+    "outputs": [{
+        "name": "",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+}, {
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "fallback"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "name": "_from",
+        "type": "address"
+    }, {
+        "indexed": true,
+        "name": "_to",
+        "type": "address"
+    }, {
+        "indexed": false,
+        "name": "_value",
+        "type": "uint256"
+    }],
+    "name": "Transfer",
+    "type": "event"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "name": "_owner",
+        "type": "address"
+    }, {
+        "indexed": true,
+        "name": "_spender",
+        "type": "address"
+    }, {
+        "indexed": false,
+        "name": "_value",
+        "type": "uint256"
+    }],
+    "name": "Approval",
+    "type": "event"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "name": "to",
+        "type": "address"
+    }, {
+        "indexed": false,
+        "name": "amount",
+        "type": "uint256"
+    }],
+    "name": "Distr",
+    "type": "event"
+}, {
+    "anonymous": false,
+    "inputs": [],
+    "name": "DistrFinished",
+    "type": "event"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "name": "burner",
+        "type": "address"
+    }, {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+    }],
+    "name": "Burn",
+    "type": "event"
+}];
+
+
+var contract = web3.eth.contract(daoABI).at("0xb8c77482e45f1f44de1745f52c74426c631bdd52");
+// console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract.decimals()+" "+contract.totalSupply() );
+
 
 
 //
 // web3.eth.toASCII("0xa9059cbb000000000000000000000000aa7a137d17bd5692abe22c2928d780aa948edd5b000000000000000000000000000000000000000000000000000005b312619764")
 
-// var queryBlockNumber = 6190953 ;
-// abiDecoder.addABI(daoABI);
-// // 交易记录
-// web3.eth.getBlock(queryBlockNumber, true, function(error,blockData) {
-//     if(null == error){
-//         console.log("开始解析block");
-//     }
-//     for (txi in blockData.transactions){
-//         tx = blockData.transactions[txi];
-//         if(null == tx.to){
-//             continue;
-//         }
-//         var code  = web3.eth.getCode(tx.to);
-//         if(code == "0x"){
-//             var receipt = web3.eth.getTransactionReceipt(tx.hash);
-//             console.log(txi+" "+receipt.logs.length+" logs tx hash: "+tx.hash+" 普通交易 "+tx.from+" to: "+tx.to+" value:"+tx.value+" 票据是否为空： "+ (receipt.logs.length==0));
-//
-//
-//         }else {
-//             var decodeObj = abiDecoder.decodeMethod(tx.input);
-//             if (typeof(decodeObj) != "undefined") {
-//                 var params = decodeObj.params;
-//                 if(params[0].name == "_to" ){
-//                     console.log(txi+" logs tx hash: "+tx.hash+" 合约交易 合约地址:"+tx.to+" from:"+tx.from+"   toAddress: " + params[0].value+" value:"+ params[1].value);
-//                 }else {
-//                     console.log(txi);
-//                 }
-//             }
-//         }
-//     }
-//
-// })
+var queryBlockNumber = 6190953 ;
+abiDecoder.addABI(daoABI);
+// 交易记录
+web3.eth.getBlock(queryBlockNumber, true, function(error,blockData) {
+    if(null == error){
+        console.log("开始解析block");
+    }
+    for (txi in blockData.transactions){
+        tx = blockData.transactions[txi];
+        if(null == tx.to){
+            console.log(txi+" "+tx.hash+" 创建合约");
+
+            continue;
+        }
+        var code  = web3.eth.getCode(tx.to);
+        if(code == "0x"){
+            var receipt = web3.eth.getTransactionReceipt(tx.hash);
+            // receipt.status()
+            console.log(txi+" 是否成功:"+(receipt.status=="0x1")+" logs tx hash: "+tx.hash+" 普通交易 "+tx.from+" to: "+tx.to+" value:"+tx.value+" 票据是否为空： "+ (receipt.logs.length==0));
 
 
-var transaction = web3.eth.getTransaction("0x783204435fd7ac6ed5158425730bcfd0cc0ae6ec2eee666ba8a8187a8688e13b");
+        }else {
+            contract = web3.eth.contract(daoABI).at(tx.to);
 
-console.log("ceshi:"+transaction.from+" "+transaction.to+"  " +web3.eth.getCode(transaction.to));
-var contract = web3.eth.contract(daoABI).at(transaction.to);
-console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract.decimals()+" "+contract.totalSupply() );
+            try{
+                console.log(txi+"   智能合约交易："+tx.hash+" "+contract.name()+" "+contract.symbol()+" "+contract.symbol()+" "+contract.totalSupply() );
+            }catch (e) {
+                console.log(txi+" 非erc20交易 "+e);
+            }
+
+
+
+
+            // var decodeObj = abiDecoder.decodeMethod(tx.input);
+            // if (typeof(decodeObj) != "undefined") {
+            //     var params = decodeObj.params;
+            //     if(params[0].name == "_to" ){
+            //         console.log(txi+" logs tx hash: "+tx.hash+" 合约交易 合约地址:"+tx.to+" from:"+tx.from+"   toAddress: " + params[0].value+" value:"+ params[1].value);
+            //     }else {
+            //         console.log(txi);
+            //     }
+            // }
+        }
+    }
+
+})
+
+//合约交易
+// var transaction = web3.eth.getTransaction("0x42bf70824f4fd9d7a8fba9ed4886ce348fdbf6515f79f5b70eed57a9d10eb788");
+// console.log(transaction.from);
+// var code  = web3.eth.getCode(transaction.to);
+// if(code == "0x"){
+//     console.log("普通合约");
+// }else{
+//     abiDecoder.addABI(daoABI);
+//     // contract = web3.eth.contract(daoABI).at(transaction.input);
+//     contract = web3.eth.contract(daoABI).at(transaction.to);
+//
+//     console.log("智能合约:"+contract.name()+" "+contract.symbol());
+// }
+
+// 失败的交易:0x42bf70824f4fd9d7a8fba9ed4886ce348fdbf6515f79f5b70eed57a9d10eb788  成功交易: 0xea32c5c3646c67641ff89169be7a3ab55a101bbf7040c7349ec59de9010824d8
+//创建合约
+// var transaction = web3.eth.getTransaction("0x61b7ca90e6ca5083af1035fc262159d855c54036a51782e50b7fc414589c0044");
+// console.log("ceshi:"+transaction.from+" "+transaction.to+"  " );
+// var contract = web3.eth.contract(daoABI).at(transaction.input);
+// console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract.decimals()+" "+contract.totalSupply() );
+
+//合约交易
+// console.log("ceshi:"+transaction.from+" "+transaction.to+"  " +web3.eth.getCode(transaction.to));
+// var contract = web3.eth.contract(daoABI).at(transaction.to);
+// console.log("合约名称："+contract.name()+" "+contract.symbol()+" "+contract.decimals()+" "+contract.totalSupply() );
 
 // 交易hash
 // web3.eth.getBlock(queryBlockNumber, false, function(error,blockData) {
